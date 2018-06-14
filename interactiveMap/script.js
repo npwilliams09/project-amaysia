af1f75cfef68c24dd7ef4eea160a03db7c77c5e7
Nathan Williams <npwilliams09@gmail.com>
2018-06-14 19:32:59 +1000

    updated files

--- interactiveMap/script.js
+++ interactiveMap/script.js
@@ -1,3 +1,18 @@
+function footerAlign() {
+    $("footer").css("display", "block");
+    $("footer").css("height", "auto");
+    var footerHeight = $("footer").outerHeight();
+    $("body").css("padding-bottom", footerHeight);
+    $("footer").css("height", footerHeight);
+ }
+
 $(document).ready(function(){
+    footerAlign();   
+    
+    $('#amap')
+        .mapster({
+            mapKey: 'data-key'
+        })
+        .mapster('set',true,'JP');
     
 });
